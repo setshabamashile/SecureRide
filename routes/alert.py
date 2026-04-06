d972db9c0ffe606aa04d0c8eca34da2013f848af
Setshaba Mashile <Setshabaseemashile@gmail.com>
2026-04-06 14:48:39 +0200

    Update alert.py

--- routes/alert.py
+++ routes/alert.py
@@ -3,7 +3,7 @@ from db import get_connection
 
 alerts = Blueprint('alerts', __name__)
 
-@users.route('/alerts', methods=['GET'])
+@alerts.route('/alerts', methods=['GET'])
 def get_alerts():
     conn = get_connection()
     cursor = conn.cursor(dictionary=True)
@@ -12,20 +12,20 @@ def get_alerts():
     conn.close()
     return jsonify(data)
 
-@users.route('/alerts/add', methods=['POST'])
+@alerts.route('/alerts/add', methods=['POST'])
 def add_alert():
     data = request.json
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute(
         "INSERT INTO Alert (alert_id, trip_id, alert_type, location, alert_time) VALUES (%s,%s,%s,%s,%s)",
-        (data['alert_id'], data['trip_id'], data['alert_type'], data['location'], data.get('alert_time', 'CURRENT_TIMESTAMP'))
+        (data['alert_id'], data['trip_id'], data['alert_type'], data['location'], data.get('alert_time'))
     )
     conn.commit()
     conn.close()
     return jsonify({"message": "Alert added"})
 
-@users.route('/alerts/update', methods=['POST'])
+@alerts.route('/alerts/update', methods=['POST'])
 def update_alert():
     data = request.json
     conn = get_connection()
@@ -38,7 +38,7 @@ def update_alert():
     conn.close()
     return jsonify({"message": "Alert updated"})
 
-@users.route('/alerts/delete', methods=['POST'])
+@alerts.route('/alerts/delete', methods=['POST'])
 def delete_alert():
     data = request.json
     conn = get_connection()
