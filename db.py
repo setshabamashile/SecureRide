4b4341a096fdaba8bfa18b957d58ab4bc4025711
Setshaba Mashile <Setshabaseemashile@gmail.com>
2026-04-08 15:12:13 +0200

    Update db.py

--- db.py
+++ db.py
@@ -2,8 +2,12 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
+        host="127.0.0.1",
+        user="3306",
+        password="22@Sizwe",
         database="secureride"
     )
+return connection
+    except Error as e:
+        print(f"Error connecting to MySQL: {e}")
+        return None
